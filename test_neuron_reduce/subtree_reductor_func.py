12b717167e6e4bd5bd798658a7ee4bfd050594ff
David Fague <98128717+davidfague@users.noreply.github.com>
2023-03-03 00:40:51 -0600

    fix indentation

--- test_neuron_reduce/subtree_reductor_func.py
+++ test_neuron_reduce/subtree_reductor_func.py
@@ -691,8 +691,8 @@ def merge_and_add_synapses(num_of_subtrees,
                     netcons_list[syn_index].setpost(PP)
                     break
             else:  # If for finish the loop -> first appearance of this synapse
-              print("x:",x,"|section_for_synapse:",section_for_synapse)  
-              synapse.loc(x, sec=section_for_synapse)
+                print("x:",x,"|section_for_synapse:",section_for_synapse)  
+                synapse.loc(x, sec=section_for_synapse)
                 new_synapses_list.append(synapse)
 
     # merging somatic and axonal synapses
